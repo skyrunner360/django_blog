c67585780c6c0fc9304470d645c04398920a9e04
skyrunner360 <mathurrishi31@gmail.com>
2020-07-06 03:03:18 +0530

    Added Tiny MCE Editor on Admin Panel

--- mendlife/writings/admin.py
+++ mendlife/writings/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from writings.models import Writing, WComment
 # Register your models here.
-admin.site.register((Writing,WComment))
+admin.site.register(WComment)
+
+@admin.register(Writing)
+class WriteAdmin(admin.ModelAdmin):
+    class Media:
+        js = ("tinyinject.js",)
